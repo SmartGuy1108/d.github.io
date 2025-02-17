b4cb2251428049f78f7bf063edc8650ab2f3b7c1
Savit <tosavitarora@gmail.com>
2025-02-16 18:06:20 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+const soundFiles = [
+    { name: 'Sound 1', file: 'sounds/sound1.mp3' },
+    { name: 'Sound 2', file: 'sounds/sound2.mp3' },
+    { name: 'Sound 3', file: 'sounds/sound3.mp3' },
+    // ... add more sounds here
+];
+
 const soundButtonsContainer = document.getElementById('sound-buttons');
 
-for (let i = 1; i <= 1000; i++) {
+soundFiles.forEach(sound => {
     const button = document.createElement('button');
-    button.innerText = `Sound ${i}`;
-    button.onclick = () => playSound(`sounds/sound${i}.mp3`);
+    button.innerText = sound.name;
+    button.onclick = () => playSound(sound.file);
     soundButtonsContainer.appendChild(button);
-}
+});
 
 function playSound(filename) {
     const audio = new Audio(filename);
